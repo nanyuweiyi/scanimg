d91afd459d1bd4dadb35e7730770bf3fffba2435
tiannannan <tiannannan@innobuddy.com>
2017-05-19 14:52:22 +0800

    init project

---
+++ app/src/main/java/com/smartstudy/zhike/scanimg/MainActivity.java
@@ -0,0 +1,15 @@
+package com.smartstudy.zhike.scanimg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
